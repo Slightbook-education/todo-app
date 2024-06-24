ee94ec6e97ce5bf717a9e5aa2e6e3e1029a3d45b
akash-cravedigi <156796896+akash-cravedigi@users.noreply.github.com>
2024-06-24 15:16:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo => todo.id===id ? {...todo, completed: !todo.completed} : todo)
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id!==id));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ToDo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
       </header>
     </div>
   );
